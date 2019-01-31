54485ef20201178c6f933859f488e619f5cb0a47
Benjamin Wittke <wittke.benjamin@web.de>
2019-01-31 12:14:58 +0100

    added LogIn Page

---
+++ SE_Fahrgemeinschaft/src/app/log-in/log-in.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { LogInPage } from './log-in.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LogInPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [LogInPage]
+})
+export class LogInPageModule {}
---
+++ SE_Fahrgemeinschaft/src/app/log-in/log-in.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-log-in',
+  templateUrl: './log-in.page.html',
+  styleUrls: ['./log-in.page.scss'],
+})
+export class LogInPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
