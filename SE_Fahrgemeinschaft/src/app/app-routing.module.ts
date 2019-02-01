f87aff730cffccfbc3e25018ac8214f6a841a9b2
Benjamin Wittke <wittke.benjamin@web.de>
2019-02-01 11:27:28 +0100

    created registration branch

--- SE_Fahrgemeinschaft/src/app/app-routing.module.ts
+++ SE_Fahrgemeinschaft/src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
   { path: 'impressum', loadChildren: './impressum/impressum.module#ImpressumPageModule' },
-  { path: 'login', loadChildren: './login/login.module#LogInPageModule' },
+  { path: 'login', loadChildren: './login/login.module#LogInPageModule' },  { path: 'registration', loadChildren: './registration/registration.module#RegistrationPageModule' },
+
 
 
 
---
+++ SE_Fahrgemeinschaft/src/app/registration/registration.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { RegistrationPage } from './registration.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: RegistrationPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [RegistrationPage]
+})
+export class RegistrationPageModule {}
