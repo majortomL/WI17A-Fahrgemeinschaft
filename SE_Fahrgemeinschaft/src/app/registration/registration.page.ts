e42a16ff227dff0fa5da6a545b3aa2e7e7d76a15
Benjamin Wittke <wittke.benjamin@web.de>
2019-02-18 13:16:03 +0100

    frontend things: info icon, placeholder, grid, checkbox

--- SE_Fahrgemeinschaft/src/app/registration/registration.page.ts
+++ SE_Fahrgemeinschaft/src/app/registration/registration.page.ts
@@ -1,42 +1,70 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthServiceService } from 'src/app/services/auth.service' ;
-
-
+import {Component, OnInit} from '@angular/core';
+import {AuthServiceService} from 'src/app/services/auth.service' ;
+import {ToastController} from "@ionic/angular";
 
 
 @Component({
-  selector: 'app-registration',
-  templateUrl: './registration.page.html',
-  styleUrls: ['./registration.page.scss'],
+    selector: 'app-registration',
+    templateUrl: './registration.page.html',
+    styleUrls: ['./registration.page.scss'],
 })
 export class RegistrationPage implements OnInit {
 
-    name: string;
-    firstname: string;
-    birthdate: number;
-    telefonnumber: number;
-    email: string;
-    emailConfirmed: string;
-    password: string;
-    passwordConfirmed: string;
+    user = {
+        name: '',
+        firstname: '',
+        birthdate: '',
+        telefonnumber: '',
+        email: '',
+        emailConfirmed: '',
+        password: '',
+        passwordConfirmed: '',
+        agbIsChecked: false,
+    };
+
 //check Types
 
-  constructor(
-      private myService: AuthServiceService
-  ) { }
+    constructor(private myService: AuthServiceService,
+                private toastCtrl: ToastController,) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
 
-  registrate(){
-   var  user = {
-            firstName: this.firstname,
-            name: this.name,
-            birthdate: this.birthdate,
-            telefon: this.telefonnumber,
-            email: this.email,
-   }
-    //provide more Information in JSON above
-   this.myService.registrate( user ,  this.password );
-  }
+    }
+
+    registrate() {
+
+        console.log(this.user);
+        //provide more Information in JSON above
+     /*   if (this.checkRegistrationSyntax()) {
+            this.myService.registrate(this.user.email, this.user.password);
+        }*/
+
+    }
+
+    checkRegistrationSyntax() {
+
+        /*Check for:
+        * empty fields
+        * tpyes
+        * matching emails
+        * optionals?
+        * AGBs
+        * */
+        //if(this.user.email == )
+        return true;
+    }
+
+    async showPasswordInfo() {
+
+        const toast = await this.toastCtrl.create({
+            message: 'Passwort muss mind. 6 Ziffern lang sein',
+            duration: 8000,
+            showCloseButton: true,
+            position: "bottom",
+            closeButtonText: 'Done'
+        });
+        toast.present(); //is working although error
+
+    }
 }
