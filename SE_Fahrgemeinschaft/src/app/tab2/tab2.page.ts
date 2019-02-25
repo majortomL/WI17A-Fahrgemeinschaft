8fe5411d6009b34d92c311bab1d5b77ef31adb1f
propagandapanda1 <phi.burkhardt.17@lehre.mosbach.dhbw.de>
2019-02-25 10:44:57 +0100

    Profile Page & Homepage improvements

--- SE_Fahrgemeinschaft/src/app/tab2/tab2.page.ts
+++ SE_Fahrgemeinschaft/src/app/tab2/tab2.page.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import {NavController} from '@ionic/angular';
+import { LoadingController, AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {}
+export class Tab2Page {
+
+  constructor(public navController: NavController) {
+  }
+
+  openProfile(){
+    //this.navController.pop('tab2');
+  }
+
+}
