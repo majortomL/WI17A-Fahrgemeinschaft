a20ceea0ebfe60b767c881911f6c4a166c3bd0cb
Benjamin Wittke <wittke.benjamin@web.de>
2019-03-02 12:47:46 +0100

    implemented "nachfrager"-page

--- SE_Fahrgemeinschaft/src/app/component/popOverRides/popOverRides.component.ts
+++ SE_Fahrgemeinschaft/src/app/component/popOverRides/popOverRides.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpParams} from "@angular/common/http";
+import {environment} from "../../../environments/environment";
+import { HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-pop-over-rides',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PopOverRidesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
   }
+
+    async Bewerben(){
+
+     await this.http.get('https://us-central1-db-test-fahrgemeinschaft.cloudfunctions.net/applyForSeat', {
+         params: new HttpParams().append('UID', environment.UID).append('RideID', "Rn257ENxyeWG7Hb4X7FufKnAwG23MosbachHeilbronn14")
+     }).subscribe((data)=>{
+         console.log(data);
+     });
+      console.log("Bewerben klappt!")
+    }
 
 }
