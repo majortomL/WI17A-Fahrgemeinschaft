a0f74385166ecaa90420aab181fa3c186e65873c
Benjamin Wittke <wittke.benjamin@web.de>
2019-03-02 12:05:15 +0100

    implemented "nachfrager"-page

--- SE_Fahrgemeinschaft/src/app/nachfrager/nachfrager.page.ts
+++ SE_Fahrgemeinschaft/src/app/nachfrager/nachfrager.page.ts
@@ -1,8 +1,19 @@
 import {Component, OnInit} from '@angular/core';
 import { RTDBService } from '../services/rtdb.service';
-import { PopoverController } from '@ionic/angular';
+import {LoadingController, PopoverController, ToastController} from '@ionic/angular';
 import { PopOverRidesComponent } from '../component/popOverRides/popOverRides.component'
 import { Router} from "@angular/router";
+
+//test for Map
+import { Platform } from '@ionic/angular'
+import {
+    GoogleMaps,
+    GoogleMap,
+    GoogleMapsEvent,
+    Marker,
+    GoogleMapsAnimation,
+    MyLocation
+} from "@ionic-native/google-maps";
 
 @Component({
     selector: 'app-nachfrager',
@@ -11,17 +22,93 @@ import { Router} from "@angular/router";
 })
 export class NachfragerPage implements OnInit {
 
+    map:GoogleMap;
+    loading: any;
+
     endTime: any;
     origin: any;
     rides : JSON [];
     constructor(private rtdb : RTDBService,
                 private popOverCtrl: PopoverController,
-                private router: Router) {
+                private router: Router,
+                private tostCtrl : ToastController,
+                private platform: Platform,
+                private loadingCtrl : LoadingController
+                ) {
 
     }
 
-    ngOnInit() {
+    async ngOnInit() {
+        await this.platform.ready();
+        await this.loadMap();
     }
+
+    loadMap() {
+        this.map = GoogleMaps.create('map_canvas', {
+            camera: {
+                target: {
+                    lat: 43.0741704,
+                    lng: -89.3809802
+                },
+                zoom: 18,
+                tilt: 30
+            }
+        });
+
+    }
+
+    async onButtonClick() {
+        this.map.clear();
+
+        this.loading = await this.loadingCtrl.create({
+            message: 'Please wait...'
+        });
+        await this.loading.present();
+
+        // Get the location of you
+        this.map.getMyLocation().then((location: MyLocation) => {
+            this.loading.dismiss();
+            console.log(JSON.stringify(location, null ,2));
+
+            // Move the map camera to the location with animation
+            this.map.animateCamera({
+                target: location.latLng,
+                zoom: 17,
+                tilt: 30
+            });
+
+            // add a marker
+            let marker: Marker = this.map.addMarkerSync({
+                title: '@ionic-native/google-maps plugin!',
+                snippet: 'This plugin is awesome!',
+                position: location.latLng,
+                animation: GoogleMapsAnimation.BOUNCE
+            });
+
+            // show the infoWindow
+            marker.showInfoWindow();
+
+            // If clicked it, display the alert
+            marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
+                this.showToast('clicked!');
+            });
+        })
+            .catch(err => {
+                this.loading.dismiss();
+                this.showToast(err.error_message);
+            });
+    }
+
+    async showToast(message: string) {
+        let toast = await this.tostCtrl.create({
+            message: message,
+            duration: 2000,
+            position: 'middle'
+        });
+
+        toast.present();
+    }
+
 
     async search() {
         //hier Algorithmus aus NotePad implementieren
