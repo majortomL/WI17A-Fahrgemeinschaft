e552cb1daee213d6dbe4cbb7786fce5d590a2111
Benjamin Wittke <wittke.benjamin@web.de>
2019-03-02 14:06:36 +0100

    implemented applyForSeat with RideID and UID(from environments)

--- SE_Fahrgemeinschaft/src/app/nachfrager/nachfrager.page.ts
+++ SE_Fahrgemeinschaft/src/app/nachfrager/nachfrager.page.ts
@@ -39,11 +39,18 @@ export class NachfragerPage implements OnInit {
         console.log("click funktioniert")
     }
 
-    async presentPopover() {
+    async presentPopover(RideID: string) {
         const popover = await this.popOverCtrl.create({
             component: PopOverRidesComponent,
-            translucent: true
+            translucent: true,
+            componentProps: {
+                "RideID" : RideID
+            }
+
         });
+        popover.onDidDismiss().then((dataReturned) => {
+
+        })
         return await popover.present();
     }
 
